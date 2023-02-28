a3788f3320243b230d3bd9c5b852610e3fbe064f
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-28 11:01:44 +0100

    fix registry

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,7 @@
 <?php
-use Tualo\Office\ExtJSCompiler\Registry;
+//use Tualo\Office\ExtJSCompiler\Registry;
 
-Registry::add(__DIR__.'/js/');
+// Registry::add(__DIR__.'/js/');
 
 if (class_exists("Tualo\Office\TualoJS\Middlewares\TualoJSMiddleware")){ }
 if (class_exists("Tualo\Office\TualoJS\Routes\TualoJSRoute")){ }
