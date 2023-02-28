5d62634235a712fcd58e07febd91d7c26e493389
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-28 11:05:09 +0100

    fix interface

--- src/Compiler.php
+++ src/Compiler.php
@@ -1,9 +1,10 @@
 <?php
 namespace Tualo\Office\TualoJS;
 
+use Tualo\Office\ExtJSCompiler\ICompiler;
 use Tualo\Office\ExtJSCompiler\CompilerHelper;
 
-class Compiler implements CompilerHelper {
+class Compiler implements ICompiler {
     
 
     public static function getFiles(){
