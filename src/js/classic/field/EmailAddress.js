ab43ac13e75bf85b4b58779927a2c2bce4a560f5
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-09-04 11:11:50 +0200

    add mail address

---
+++ src/js/classic/field/EmailAddress.js
@@ -0,0 +1,37 @@
+Ext.define('Ext.tualo.form.field.EmailAddress',  {
+    alias: 'widget.tualomailaddress',
+    extend: 'Ext.form.field.Text',
+    /*
+    ,
+    */
+    onChange: function(newVal, oldVal) {
+        var me = this;
+        document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","mail:"+me.getValue());
+        console.log(me.getValue());
+        if (me.getValue()==''){
+            document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","javascript: void(0)");
+        }
+        me.callParent([newVal, oldVal]);
+    },
+    triggers: {
+        makeacall: {
+            cls: 'fa-phone',
+            renderTpl: [
+                '<a href="javascript: void(0)" id="{triggerId}" class="{baseCls} {baseCls}-{ui} {cls} {cls}-{ui} {extraCls} ',
+                        '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
+                        '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
+                    '>',
+                    '{[values.$trigger.renderBody(values)]}',
+                '</a>'
+            ],
+            /*
+            bodyTpl: [
+                '<a href="tel:+491734090944"  id="{triggerId}-link"><i class="fa fa-phone"></i></a>'
+            ]*/
+            handler: function(me) {
+                
+                
+            }
+        }
+    }
+});
