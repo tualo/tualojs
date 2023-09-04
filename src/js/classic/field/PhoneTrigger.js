87f1856d835282135836a28c301837e1d296d178
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-09-04 10:56:27 +0200

    add PhoneTrigger

---
+++ src/js/classic/field/PhoneTrigger.js
@@ -0,0 +1,18 @@
+Ext.define('Ext.tualo.form.field.Phonetrigger',  {
+    alias: 'widget.phonetrigger',
+    extend: 'Ext.form.field.Text',
+    constructor: function(config) {
+    	config = config || {};
+      Ext.apply(this,config);
+      //config.readOnly=true,
+	    this.callParent([config]);
+    },
+    triggers: {
+        phoncall: {
+            cls: 'fa fa-phone',
+            handler: function() {
+                console.log('foo trigger clicked');
+            }
+        }
+    }
+});
