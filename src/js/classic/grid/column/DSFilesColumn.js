ad55fdda1f171ace3c8b701c9139e0780d80f188
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-09-19 11:16:06 +0200

    add linke ds files column

---
+++ src/js/classic/grid/column/DSFilesColumn.js
@@ -0,0 +1,17 @@
+Ext.define('Ext.tualo.grid.column.DSFilesColumn', {
+    extend: 'Ext.grid.column.Column',
+    alias: 'widget.tualodsfilescolumn',
+    align: 'left',
+   
+    /*cellWrap: true,
+    variableRowHeight: true,
+    */
+    renderer: function(val,meta,rec){
+        let file_id = val,tn=rec.get('__table_name');
+        if (typeof rec.get('__file_id')=='string'){
+            file_id = rec.get('__file_id');
+            return '<a href="/dsfiles/'+tn+'/'+file_id+'" target="_blank">'+rec.get('__file_name')+'</a>';
+        }
+        return "";
+    }
+});
