a0d18c80aed705f41d48bee8ec28a0a9a1a78838
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-09-22 08:38:47 +0200

    add custom types to sql

--- src/sql/custom_types.sql
+++ src/sql/custom_types.sql
@@ -1786,3 +1786,6 @@ values
         0
     )
 ;
+
+
+insert ignore into custom_types (        vendor,        name,        id, xtype_long_classic, extendsxtype_classic,        xtype_long_modern,        extendsxtype_modern    ) values ( "Tualo",  "Ext.tualo.grid.column.FileSizeDisplayColumn", "Ext.tualo.grid.column.FileSizeDisplayColumn", "widget.tualodisplayfilesize", "Ext.grid.column.Column", "widget.textarea", "Ext.field.Text" ) on duplicate key update    id =values(id), xtype_long_classic = values(xtype_long_classic),    extendsxtype_classic = values(extendsxtype_classic),name = values(name),vendor = values(vendor);
