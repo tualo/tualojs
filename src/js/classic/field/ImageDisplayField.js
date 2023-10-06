6477247caf47ba79cd6f911ca48726d7642e260e
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-06 11:58:01 +0200

    use try catch

--- src/js/classic/field/ImageDisplayField.js
+++ src/js/classic/field/ImageDisplayField.js
@@ -15,48 +15,18 @@ Ext.define('Ext.tualo.form.field.ImageDisplayField', {
 
   setValue: function(v) {
     this.loadFile(v);
-    //this.setRawValue(v);
-    //this.field.setValue(v);
   },
   getValue: function() {
     return this.getRawValue();
-    //return this.field.getValue();
   },
   height: 45,
   loadFile: function(name) {
-    var me = this;
-//    me.mimetype = 'image/png';
-    var el = document.getElementById(me.id+'-bodyEl');
-    el.innerHTML = '<img width="100%" style="max-width:300px;" src="'+ name +'"/>';
-
-    /*
-    Ext.Ajax.request({
-      url: './in dex.php',//?sid=" + sid + '&b=' + this.parent.index + '&cmp=cmp_bezugstamm&TEMPLATE=NO&p=ajax/utils/file/mime',
-      params: {
-        sid: sid,
-        t: this.tablename,
-        cmp: 'cmp_ds',
-        p: 'file',
-        decode: '0',
-        TEMPLATE: 'NO',
-        id: id,
-        maxwidth: me.getWidth()*4,
-        maxheight: me.getHeight()*4
-      },
-      scope: this,
-      success: function(f, o) {
-        if (me.rendered) {
-          var width= me.getWidth();
-          var height=me.getHeight();
-          var el = document.getElementById(me.id+'-bodyEl');
-          if (f.responseText!==''){
-            el.innerHTML = '<img width="100%" src="data:' + me.mimetype+';base64,'+ f.responseText +'"/>';
-          }else{
-            el.innerHTML = 'kein Dokument';
-          }
-        }
-      }
-    });
-    */
+    try{
+      var me = this;
+      var el = document.getElementById(me.id+'-bodyEl');
+      el.innerHTML = '<img width="100%" style="max-width:300px;" src="'+ name +'"/>';
+    }catch(e){
+      console.log(e);
+    }
   }
 });
