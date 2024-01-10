39879fc265a14abef5c0102725be76c9173b7e98
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-10 09:00:48 +0100

    add tualotimedisplayfield

--- src/js/classic/field/DatetimeDisplayField.js
+++ src/js/classic/field/DatetimeDisplayField.js
@@ -6,3 +6,13 @@ Ext.define('Ext.tualo.form.field.DatetimeDisplayField',  {
       return Ext.util.Format.date(value,'d.m.Y H:i');
     }
 });
+
+
+Ext.define('Ext.tualo.form.field.TimeDisplayField',  {
+  alias: 'widget.tualotimedisplayfield',
+  extend: 'Ext.form.field.Display',
+
+  renderer: function(value, field){
+    return Ext.util.Format.date(value,'H:i:s');
+  }
+});
