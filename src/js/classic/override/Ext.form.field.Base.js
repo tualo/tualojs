49c9400ac75e27735a40f236b86cafa41ef1cbdd
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-03-08 20:50:55 +0100

    fixes

--- src/js/classic/override/Ext.form.field.Base.js
+++ src/js/classic/override/Ext.form.field.Base.js
@@ -1,4 +1,15 @@
 
+
+Ext.define('Tualo.Ext.grid.filters.filter.Base', {
+    override :'Ext.grid.filters.filter.Base',
+    acceptedType: 'any'
+});
+
+Ext.define('Tualo.Ext.grid.filters.filter.List', {
+    override :'Ext.grid.filters.filter.List',
+    acceptedType: 'array'
+})
+
 Ext.define('Tualo.Ext.form.field.Base', {
     override :'Ext.form.field.Base',
 
---
+++ src/js/classic/override/Field.js
@@ -0,0 +1,9 @@
+Ext.define('Tualo.Ext.grid.filters.filter.Base', {
+    override :'Ext.grid.filters.filter.Base',
+    acceptedType: 'any'
+});
+
+Ext.define('Tualo.Ext.grid.filters.filter.List', {
+    override :'Ext.grid.filters.filter.List',
+    acceptedType: 'array'
+});
