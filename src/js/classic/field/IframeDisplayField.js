15869e710f7132608a14586b446c5b562f654f3a
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2024-11-21 11:23:58 +0100

    add iframe

---
+++ src/js/classic/field/IframeDisplayField.js
@@ -0,0 +1,48 @@
+Ext.define('Ext.tualo.form.field.IframeDisplayField', {
+  extend:'Ext.form.field.Display',
+  alias: 'widget.iframedisplayfield',
+  requires: ['Ext.util.Format', 'Ext.XTemplate'],
+  fieldSubTpl: [
+      '<div id="{id}" data-ref="inputEl" tabindex="-1" role="textbox" aria-readonly="true"',
+      ' aria-labelledby="{cmpId}-labelEl" {inputAttrTpl}',
+      '<tpl if="fieldStyle"> style="width:100%;{fieldStyle}"</tpl>',
+      ' class="{fieldCls} {fieldCls}-{ui}"><div id="{id}-image"></div></div>',
+      {
+          compiled: true,
+          disableFormats: true
+      }
+  ],
+
+  setValue: function(v) {
+
+    this.loadFile(v);
+  },
+  getValue: function() {
+    return this.getRawValue();
+  },
+  height: 45,
+  loadFile: function(name) {
+    try{
+      console.log('loadFile',name);
+      var me = this;
+      var el = document.getElementById(me.id+'-bodyEl');
+      el.innerHTML = '<iframe width="100%" style="max-width:300px;" src="'+ name +'"></iframe>';
+    }catch(e){
+      console.log(e);
+    }
+  },
+  onDisable: function() {
+    try{
+      this.callParent(arguments);
+    }catch(e){
+      console.log('*',e);
+    }
+  },
+  onEnable: function() {
+    try{
+      this.callParent(arguments);
+    }catch(e){
+      console.log('*',e);
+    }
+  }
+});
