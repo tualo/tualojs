02c1c06dc4ca5fbc1cdd8560e238ff83d4d0c79d
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-07 10:03:40 +0200

    check if file exists

--- src/Routes/DSFields.php
+++ src/Routes/DSFields.php
@@ -1,32 +1,51 @@
 <?php
+
 namespace Tualo\Office\TualoJS\Routes;
+
 use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\Basic\Route;
 use Tualo\Office\Basic\IRoute;
 
 
-class DSFields implements IRoute{
-    public static function register(){
-        Route::add('/tualocombo/dsfields',function(){
+class DSFields implements IRoute
+{
+    public static function register()
+    {
+        Route::add(
+            '/tualocombo/dsfields',
+            function () {
 
-        $db = TualoApplication::get('session')->getDB();
-        $session = TualoApplication::get('session');
-        try {
-            
-            $sql = 'select column_name id  from ds_column  ';
-            if (isset($_REQUEST['tablename'])){
-            $sql .= ' where  table_name = {tablename} ';
-            }
-            $data  = $db->direct($sql,$_REQUEST);
-            TualoApplication::result('data', $data );
-            TualoApplication::result('total', count($data) );
-            TualoApplication::result('success', true );
-            
-        }catch(\Exception $e){
-            TualoApplication::result('msg', $e->getMessage());
-        }
-        TualoApplication::contenttype('application/json');
+                $db = TualoApplication::get('session')->getDB();
+                $session = TualoApplication::get('session');
+                try {
 
-        },array('get','post'),true);
+                    $sql = 'select column_name id  from ds_column  ';
+                    if (isset($_REQUEST['tablename'])) {
+                        $sql .= ' where  table_name = {tablename} ';
+                    }
+                    $data  = $db->direct($sql, $_REQUEST);
+                    TualoApplication::result('data', $data);
+                    TualoApplication::result('total', count($data));
+                    TualoApplication::result('success', true);
+                } catch (\Exception $e) {
+                    TualoApplication::result('msg', $e->getMessage());
+                }
+                TualoApplication::contenttype('application/json');
+            },
+            array('get', 'post'),
+            true,
+            [
+                'errorOnUnexpected' => true,
+                'errorOnInvalid' => true,
+                'fields' => [
+                    'tablename' => [
+                        'required' => false,
+                        'type' => 'string',
+                        'max_length' => 50,
+                        'pattern' => '/^[a-zA-ZäöüÄÖÜß\w\d]+$/u'
+                    ]
+                ]
+            ]
+        );
     }
 }
--- src/Routes/Download.php
+++ src/Routes/Download.php
@@ -1,44 +1,55 @@
 <?php
+
 namespace Tualo\Office\TualoJS\Routes;
+
 use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\Basic\Route;
 use Tualo\Office\Basic\IRoute;
 
 
-class Download implements IRoute{
-    public static function register(){
+class Download implements IRoute
+{
+    public static function register()
+    {
 
 
-            
-        Route::add('/tualo/download/(?P<file>[\w.\/\-]+)',function($matches){
-            try{
+
+        Route::add('/tualo/download/(?P<file>[\w.\/\-]+)', function ($matches) {
+            try {
                 TualoApplication::contenttype('text/html');
-                if (!file_exists(TualoApplication::get('tempPath').'/'.$matches['file'])){
+                if (!file_exists(TualoApplication::get('tempPath') . '/' . $matches['file'])) {
                     TualoApplication::contenttype('text/html');
                     header("HTTP/1.0 404 Not Found");
                     exit();
-                }else{
+                } else {
+                    if (strpos($matches['file'], '..') !== false || strpos($matches['file'], '/') === 0) {
+                        TualoApplication::contenttype('text/html');
+                        header("HTTP/1.0 403 Forbidden");
+                        exit();
+                    }
+                    if (!file_exists(TualoApplication::get('tempPath') . '/' . $matches['file'])) {
+                        TualoApplication::contenttype('text/html');
+                        header("HTTP/1.0 404 Not Found");
+                        exit();
+                    }
 
-                    $size = filesize(TualoApplication::get('tempPath').'/'.$matches['file']);
+                    $size = filesize(TualoApplication::get('tempPath') . '/' . $matches['file']);
 
                     header('Content-Description: File Transfer');
-                    header('Content-Type: '.mime_content_type(TualoApplication::get('tempPath').'/'.$matches['file']));
-                    header('Content-Disposition: attachment; filename="'.$matches['file'].'"');
+                    header('Content-Type: ' . mime_content_type(TualoApplication::get('tempPath') . '/' . $matches['file']));
+                    header('Content-Disposition: attachment; filename="' . $matches['file'] . '"');
                     header('Content-Transfer-Encoding: binary');
                     header('Expires: 0');
                     header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
                     header('Pragma: public');
                     header('Content-Length: ' . $size);
 
-                    readfile(TualoApplication::get('tempPath').'/'.$matches['file']);
+                    readfile(TualoApplication::get('tempPath') . '/' . $matches['file']);
                     exit();
                 }
-
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo $e->getMessage();
             }
-        },['get','post'],true);
-       
-
+        }, ['get', 'post'], true, []);
     }
 }
