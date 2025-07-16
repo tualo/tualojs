d984d169352aeff6b8403ca1b77dd68fdf256743
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-16 11:15:59 +0200

    add trigger route

---
+++ src/Routes/DSTrigger.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tualo\Office\TualoJS\Routes;
+
+use Tualo\Office\Basic\TualoApplication;
+use Tualo\Office\Basic\Route;
+use Tualo\Office\Basic\IRoute;
+use Tualo\Office\DS\Routes\DS;
+
+
+class DSTrigger implements IRoute
+{
+    public static function register()
+    {
+        Route::add(
+            '/tualocombo/dstrigger',
+            function () {
+
+                $db = TualoApplication::get('session')->getDB();
+                $session = TualoApplication::get('session');
+                try {
+
+                    $data  = [];
+                    TualoApplication::result('data', $data);
+                    TualoApplication::result('total', count($data));
+                    TualoApplication::result('success', true);
+                } catch (\Exception $e) {
+                    TualoApplication::result('msg', $e->getMessage());
+                }
+                TualoApplication::contenttype('application/json');
+            },
+            array('get', 'post'),
+            true,
+            [
+                'errorOnUnexpected' => true,
+                'errorOnInvalid' => true,
+                'fields' => DS::DefaultExpectedFields
+            ]
+        );
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,7 @@
 
 require_once "Compiler.php";
 require_once "Routes/DSFields.php";
+require_once "Routes/DSTrigger.php";
 require_once "Routes/Download.php";
 require_once "Commands/Install.php";
 require_once "Commands/Setup.php";
