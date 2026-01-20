366bc937eb6e82dc6de5c08879c6f3358c929084
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-01-20 15:22:35 +0100

    Version bump to 1.3.6

--- src/js/classic/field/EmailAddress.js
+++ src/js/classic/field/EmailAddress.js
@@ -1,14 +1,15 @@
-Ext.define('Ext.tualo.form.field.EmailAddress',  {
+Ext.define('Ext.tualo.form.field.EmailAddress', {
     alias: 'widget.tualomailaddress',
     extend: 'Ext.form.field.Text',
     /*
     ,
     */
-    onChange: function(newVal, oldVal) {
+    onChange: function (newVal, oldVal) {
         var me = this;
-        document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","mailto:"+me.getValue());
-        if (me.getValue()==''){
-            document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","javascript: void(0)");
+        if (!document.getElementById(me.id + '-trigger-makeacall')) return;
+        document.getElementById(me.id + '-trigger-makeacall').setAttribute("href", "mailto:" + me.getValue());
+        if (me.getValue() == '') {
+            document.getElementById(me.id + '-trigger-makeacall').setAttribute("href", "javascript: void(0)");
         }
         me.callParent([newVal, oldVal]);
     },
@@ -17,30 +18,31 @@ Ext.define('Ext.tualo.form.field.EmailAddress',  {
             cls: 'fa-at',
             renderTpl: [
                 '<a href="javascript: void(0)" id="{triggerId}" class="{baseCls} {baseCls}-{ui} {cls} {cls}-{ui} {extraCls} ',
-                        '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
-                        '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
-                    '>',
-                    '{[values.$trigger.renderBody(values)]}',
+                '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
+                '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
+                '>',
+                '{[values.$trigger.renderBody(values)]}',
                 '</a>'
             ],
-            
-            handler: function(me) {
-                
-                
+
+            handler: function (me) {
+
+
             }
         }
     }
 });
 
-Ext.define('Ext.tualo.form.field.EmailAddressReadonly',  {
+Ext.define('Ext.tualo.form.field.EmailAddressReadonly', {
     alias: 'widget.tualomailaddressreadonly',
     extend: 'Ext.form.field.Text',
     readOnly: true,
-    onChange: function(newVal, oldVal) {
+    onChange: function (newVal, oldVal) {
         var me = this;
-        document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","mailto:"+me.getValue());
-        if (me.getValue()==''){
-            document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","javascript: void(0)");
+        if (!document.getElementById(me.id + '-trigger-makeacall')) return;
+        document.getElementById(me.id + '-trigger-makeacall').setAttribute("href", "mailto:" + me.getValue());
+        if (me.getValue() == '') {
+            document.getElementById(me.id + '-trigger-makeacall').setAttribute("href", "javascript: void(0)");
         }
         me.callParent([newVal, oldVal]);
     },
@@ -49,16 +51,16 @@ Ext.define('Ext.tualo.form.field.EmailAddressReadonly',  {
             cls: 'fa-at',
             renderTpl: [
                 '<a href="javascript: void(0)" id="{triggerId}" class="{baseCls} {baseCls}-{ui} {cls} {cls}-{ui} {extraCls} ',
-                        '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
-                        '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
-                    '>',
-                    '{[values.$trigger.renderBody(values)]}',
+                '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
+                '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
+                '>',
+                '{[values.$trigger.renderBody(values)]}',
                 '</a>'
             ],
-            
-            handler: function(me) {
-                
-                
+
+            handler: function (me) {
+
+
             }
         }
     }
@@ -67,11 +69,11 @@ Ext.define('Ext.tualo.form.field.EmailAddressReadonly',  {
 
 
 Ext.merge(Ext.util.Format, {
-    emailLinkRenderer: function(val,meta,rec){
+    emailLinkRenderer: function (val, meta, rec) {
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (emailPattern.test(val) ){
-            return '<a href="mailto:'+val+'">'+val+'</a>';
-        }else{
+        if (emailPattern.test(val)) {
+            return '<a href="mailto:' + val + '">' + val + '</a>';
+        } else {
             return val;
         }
     }
--- src/js/classic/field/PhoneNumber.js
+++ src/js/classic/field/PhoneNumber.js
@@ -1,14 +1,15 @@
-Ext.define('Ext.tualo.form.field.PhoneNumber',  {
+Ext.define('Ext.tualo.form.field.PhoneNumber', {
     alias: 'widget.tualophonenumber',
     extend: 'Ext.form.field.Text',
     /*
     ,
     */
-    onChange: function(newVal, oldVal) {
+    onChange: function (newVal, oldVal) {
         var me = this;
-        document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","tel:"+me.getValue());
-        if (me.getValue()==''){
-            document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","javascript: void(0)");
+        if (!document.getElementById(me.id + '-trigger-makeacall')) return;
+        document.getElementById(me.id + '-trigger-makeacall').setAttribute("href", "tel:" + me.getValue());
+        if (me.getValue() == '') {
+            document.getElementById(me.id + '-trigger-makeacall').setAttribute("href", "javascript: void(0)");
         }
         me.callParent([newVal, oldVal]);
     },
@@ -17,31 +18,32 @@ Ext.define('Ext.tualo.form.field.PhoneNumber',  {
             cls: 'fa-phone',
             renderTpl: [
                 '<a href="javascript: void(0)" id="{triggerId}" class="{baseCls} {baseCls}-{ui} {cls} {cls}-{ui} {extraCls} ',
-                        '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
-                        '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
-                    '>',
-                    '{[values.$trigger.renderBody(values)]}',
+                '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
+                '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
+                '>',
+                '{[values.$trigger.renderBody(values)]}',
                 '</a>'
             ],
-            
-            handler: function(me) {
-                
-                
+
+            handler: function (me) {
+
+
             }
         }
     }
 })
 
 
-Ext.define('Ext.tualo.form.field.PhoneNumberReadonly',  {
+Ext.define('Ext.tualo.form.field.PhoneNumberReadonly', {
     alias: 'widget.tualophonenumberreadonly',
     extend: 'Ext.form.field.Text',
     readOnly: true,
-    onChange: function(newVal, oldVal) {
+    onChange: function (newVal, oldVal) {
         var me = this;
-        document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","tel:"+me.getValue());
-        if (me.getValue()==''){
-            document.getElementById(me.id+'-trigger-makeacall').setAttribute("href","javascript: void(0)");
+        if (!document.getElementById(me.id + '-trigger-makeacall')) return;
+        document.getElementById(me.id + '-trigger-makeacall').setAttribute("href", "tel:" + me.getValue());
+        if (me.getValue() == '') {
+            document.getElementById(me.id + '-trigger-makeacall').setAttribute("href", "javascript: void(0)");
         }
         me.callParent([newVal, oldVal]);
     },
@@ -50,28 +52,28 @@ Ext.define('Ext.tualo.form.field.PhoneNumberReadonly',  {
             cls: 'fa-phone',
             renderTpl: [
                 '<a href="javascript: void(0)" id="{triggerId}" class="{baseCls} {baseCls}-{ui} {cls} {cls}-{ui} {extraCls} ',
-                        '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
-                        '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
-                    '>',
-                    '{[values.$trigger.renderBody(values)]}',
+                '{childElCls}" style="width: 32px;<tpl if="triggerStyle">{triggerStyle}</tpl>"',
+                '<tpl if="ariaRole"> role="{ariaRole}"<tpl else> role="presentation"</tpl>',
+                '>',
+                '{[values.$trigger.renderBody(values)]}',
                 '</a>'
             ],
-            
-            handler: function(me) {
-                
-                
+
+            handler: function (me) {
+
+
             }
         }
     }
 })
 
 Ext.merge(Ext.util.Format, {
-    phonenumberLinkRenderer: function(val,meta,rec){
+    phonenumberLinkRenderer: function (val, meta, rec) {
         const validatePhoneNumberRegex = /^\+?[0-9][0-9]{7,14}$/;
-        let simple = val.replace(/[^0-9\+]/g,'')
-        if ( validatePhoneNumberRegex.test(simple) ){
-            return '<a href="tel:'+simple+'">'+val+'</a>';
-        }else{
+        let simple = val.replace(/[^0-9\+]/g, '')
+        if (validatePhoneNumberRegex.test(simple)) {
+            return '<a href="tel:' + simple + '">' + val + '</a>';
+        } else {
             return val;
         }
     }
