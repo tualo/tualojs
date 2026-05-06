ff12cfe4548356e9c2dd269aa0b2d02a60a793ab
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-05-06 19:53:04 +0200

    Version bump to 1.3.34

--- src/js/both/data/calculation/field/Einheit.js
+++ src/js/both/data/calculation/field/Einheit.js
@@ -11,30 +11,33 @@ Ext.define('Tualo.tualojs.data.calculation.field.Einheit', {
         if (!Ext.isEmpty(record.get('artikel'))) {
             if (typeof record.modified == 'undefined') return currentValue * 1;
             if (record.modified) {
-                let type = record.get('artikel');
-                let request = await fetch('./ds/mengeneinheiten/read/' + type);
-                if (!request.ok) {
-                    Ext.toast({
-                        html: 'Fehler beim Abrufen ' + type + ' Daten',
-                        title: 'Fehler',
-                        width: 200,
-                        align: 't'
-                    });
-                    return;
-                }
-                let data = await request.json();
-                if (!data.success) {
-                    let msg = data.msg;
-                    if (!msg) msg = "Leider ist ein unbekannter Fehler aufgetreten.";
-                    Ext.toast({
-                        html: msg,
-                        title: 'Fehler',
-                        width: 200,
-                        align: 't'
-                    });
-                    return;
-                }
-                record.set('einheit', data.data[0].faktor * 1.0);
+                let type = encodeURIComponent(record.get('artikel'));
+                let fn = async () => {
+                    let request = await fetch('./ds/view_artikelgruppen_einheit/read/' + type);
+                    if (!request.ok) {
+                        Ext.toast({
+                            html: 'Fehler beim Abrufen ' + type + ' Daten',
+                            title: 'Fehler',
+                            width: 200,
+                            align: 't'
+                        });
+                        return;
+                    }
+                    let data = await request.json();
+                    if (!data.success) {
+                        let msg = data.msg;
+                        if (!msg) msg = "Leider ist ein unbekannter Fehler aufgetreten.";
+                        Ext.toast({
+                            html: msg,
+                            title: 'Fehler',
+                            width: 200,
+                            align: 't'
+                        });
+                        return;
+                    }
+                    record.set('einheit', data.data[0].faktor * 1.0);
+                };
+                fn();
             }
         }
     },
--- src/js/both/data/calculation/field/EinheitFaktor.js
+++ src/js/both/data/calculation/field/EinheitFaktor.js
@@ -12,29 +12,32 @@ Ext.define('Tualo.tualojs.data.calculation.field.EinheitFaktor', {
             if (typeof record.modified == 'undefined') return currentValue * 1;
             if (record.modified) {
                 let type = record.get('einheit');
-                let request = await fetch('./ds/mengeneinheiten/read/' + type);
-                if (!request.ok) {
-                    Ext.toast({
-                        html: 'Fehler beim Abrufen ' + type + ' Daten',
-                        title: 'Fehler',
-                        width: 200,
-                        align: 't'
-                    });
-                    return;
-                }
-                let data = await request.json();
-                if (!data.success) {
-                    let msg = data.msg;
-                    if (!msg) msg = "Leider ist ein unbekannter Fehler aufgetreten.";
-                    Ext.toast({
-                        html: msg,
-                        title: 'Fehler',
-                        width: 200,
-                        align: 't'
-                    });
-                    return;
-                }
-                record.set('einheit_faktor', data.data[0].faktor * 1.0);
+                let fn = async () => {
+                    let request = await fetch('./ds/mengeneinheiten/read/' + type);
+                    if (!request.ok) {
+                        Ext.toast({
+                            html: 'Fehler beim Abrufen ' + type + ' Daten',
+                            title: 'Fehler',
+                            width: 200,
+                            align: 't'
+                        });
+                        return;
+                    }
+                    let data = await request.json();
+                    if (!data.success) {
+                        let msg = data.msg;
+                        if (!msg) msg = "Leider ist ein unbekannter Fehler aufgetreten.";
+                        Ext.toast({
+                            html: msg,
+                            title: 'Fehler',
+                            width: 200,
+                            align: 't'
+                        });
+                        return;
+                    }
+                    record.set('einheit_faktor', data.data[0].faktor * 1.0);
+                };
+                fn();
             }
         }
     },
